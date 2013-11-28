29cda38fbb6a1158e15334a726f3a7b85dea6969
Erasmo <erasmo.oliveira87@gmail.com>
2013-11-28 16:27:04 -0200

    First commit
    First commit

---
+++ WindowsFormsApplication3/CloseMedia.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace close
+{
+    class CloseMedia
+    {
+
+        [DllImport("winmm.dll")]
+        static extern Int32 mciSendString(String command, StringBuilder buffer, Int32 bufferSize, IntPtr hwndCallback);
+
+        public static void Open()
+        {
+            // Open the door
+            mciSendString("set CDAudio door open", null, 0, IntPtr.Zero);
+        }
+
+        public static void Close()
+        {
+            // Close the door
+            mciSendString("set CDAudio door closed", null, 0, IntPtr.Zero);
+        }
+
+    }
+}
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using close;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        private bool isOpen = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            setDataField();
+        }
+        
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            if (isOpen == false)
+            {
+                CloseMedia.Open();
+                isOpen = true;
+                btnAbrir.Text = "Close";
+            }
+            else
+            {
+                CloseMedia.Close();
+                isOpen = false;
+                btnAbrir.Text = "Open";
+            }
+            
+        }
+
+        private void setDataField()
+        {
+            btnAbrir.Text = "Open";
+        }
+    }
+}
